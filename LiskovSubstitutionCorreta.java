c50900ba2c4e3568931e1e37aad72b5a2d2aab02
brainner0 <brainner.trabalho@gmail.com>
2025-02-20 13:02:07 -0300

    Update LiskovSubstitutionCorreta.java

--- LiskovSubstitutionCorreta.java
+++ LiskovSubstitutionCorreta.java
@@ -9,24 +9,20 @@ public class LiskovSubstitutionCorreta {
     }
 }
 
-// Interface para aves que voam
 interface FlyingBird {
     void fly();
 }
 
-// Interface para aves que não voam
 interface NonFlyingBird {
     void walk();
 }
 
-// Implementação para um pássaro que voa
 class Sparrow implements FlyingBird {
     public void fly() {
         System.out.println("Sparrow is flying");
     }
 }
 
-// Implementação para um pássaro que não voa
 class Ostrich implements NonFlyingBird {
     public void walk() {
         System.out.println("Ostrich is walking");
